a368f38d129884bdc867b490a94ec63d5560ba38
sercangunonu <sercangunonu@gmail.com>
2024-09-24 18:41:57 +0300

    Initial Commit

---
+++ Akakce/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Akakce
+//
+//  Created by Sercan Deniz on 24.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
